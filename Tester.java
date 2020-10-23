34ed74ca7597134b598d1d5e4844feb95ab65e29
i has candys <ichen10@stuy.edu>
2020-10-23 13:18:54 -0400

    largest works now

--- Tester.java
+++ Tester.java
@@ -1,6 +1,20 @@
 public class Tester {
 	public static void main(String[] args) {
+		int[]  A  =   {  1, 12, 23 };
 		int[]  B  =   {  1, 3, 5 };
+		int[]  C  = {50};
+
+		System.out.println("sum test cases:");
+		System.out.println(ArrayOps.sum(A));
 		System.out.println(ArrayOps.sum(B));
+		System.out.println(ArrayOps.sum(C));
+		System.out.println();
+
+		System.out.println("largest test cases:");
+		System.out.println(ArrayOps.largest(A));
+		System.out.println(ArrayOps.largest(B));
+		System.out.println(ArrayOps.largest(C));
+		System.out.println();
+
     }
 }
